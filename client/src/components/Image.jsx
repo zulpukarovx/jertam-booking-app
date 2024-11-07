a030bd188c646b9936b8d711bdc91bfb748f800c
zulpukarovx <zulpukarovx@gmail.com>
2024-11-08 00:19:56 +0100

    Image component synced with api

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,6 +1,5 @@
 
 const Image = ({src, className, ...rest}) => {
-    src = src && src.includes('https://') ? src : 'http://localhost:4000/uploads/' + src;
   return (
     <img className={className} {...rest} src={src} alt="" />
   );
